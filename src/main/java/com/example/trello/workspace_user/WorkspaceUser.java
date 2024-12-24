9848556499fd5fd7fc91f7518d1d66fc0f75c8b6
Newbiekk <newbiekk1126@gmail.com>
2024-12-24 18:47:30 +0900

    fix : WorkspaceUser 엔티티 수정
    역할 관리를 위한 Role 컬럼 추가

--- src/main/java/com/example/trello/workspace_user/WorkspaceUser.java
+++ src/main/java/com/example/trello/workspace_user/WorkspaceUser.java
@@ -1,7 +1,9 @@
 package com.example.trello.workspace_user;
 
 import com.example.trello.user.User;
+import com.example.trello.workspace.Workspace;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 
 @Entity
@@ -12,11 +14,22 @@ public class WorkspaceUser {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "role")
+    private WorkspaceUserRole role;
+
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    private WorkspaceUser workspaceUser;
+    private Workspace workspace;
 
+    public WorkspaceUser() {
+    }
 
+    @Builder
+    public WorkspaceUser(WorkspaceUserRole role, User user, Workspace workspace) {
+        this.role = role;
+        this.user = user;
+        this.workspace = workspace;
+    }
 }
