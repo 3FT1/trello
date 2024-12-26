826f304354d72b5bc12444bbf3aa5037427c9b0c
2unmi <2unmin4030@gmail.com>
2024-12-26 21:04:45 +0900

    fix : 각 요구 사항에 맞는 Update, Delete 수정

--- src/main/java/com/example/trello/cardlist/CardListService.java
+++ src/main/java/com/example/trello/cardlist/CardListService.java
@@ -2,9 +2,9 @@ package com.example.trello.cardlist;
 
 import com.example.trello.board.Board;
 import com.example.trello.board.BoardRepository;
-import com.example.trello.cardlist.dto.CardListRequestDto;
+import com.example.trello.cardlist.dto.UpdateCardListRequestDto;
 import com.example.trello.cardlist.dto.CreateCardListRequestDto;
-import com.example.trello.cardlist.dto.CreateCardListResponseDto;
+import com.example.trello.cardlist.dto.CardListResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,28 +18,57 @@ public class CardListService {
     private final CardListRepository cardListRepository;
     private final BoardRepository boardRepository;
 
-    private static Integer MAX_SEQUENCE;
+    private Integer maxSequence; // 보드에서 가장 큰 순서
 
     @Transactional
-    public CreateCardListResponseDto create(CreateCardListRequestDto requestDto) {
+    public CardListResponseDto create(CreateCardListRequestDto requestDto) {
         Board board = boardRepository.findByIdOrElseThrow(requestDto.getBoardId());
-        MAX_SEQUENCE = cardListRepository.findByMax().orElse(0);
-        CardList cardList = requestDto.toEntity(requestDto, board, ++MAX_SEQUENCE);
+        maxSequence = cardListRepository.findByMax(board.getId()).orElse(0);
+        CardList cardList = requestDto.toEntity(requestDto, board, ++maxSequence);
         CardList savedCardList = cardListRepository.save(cardList);
 
         return CardList.toDto(savedCardList);
     }
 
 
-    public CreateCardListResponseDto findCardList(Long id) {
+    public CardListResponseDto findCardList(Long id) { //todo dto 이름 바꾸기
         CardList cardList = cardListRepository.findByIdOrElseThrow(id);
         return CardList.toDto(cardList);
     }
 
     @Transactional
-    public CreateCardListResponseDto update(Long id, CardListRequestDto requestDto) {
+    public CardListResponseDto moveSequence(Long id, UpdateCardListRequestDto requestDto) {
         CardList cardList = cardListRepository.findByIdOrElseThrow(id);
-        CardList exchangeCardList = cardListRepository.findBySequenceOrElseThrow(requestDto.getSequence());
+        Integer currentSequence=cardList.getSequence();
+
+        List<CardList> cardLists = cardListRepository.findAllByBoardId(cardList.getBoard().getId());
+
+        if(currentSequence!=requestDto.getSequence()) {
+            if(currentSequence<requestDto.getSequence()) {
+                cardLists.stream()
+                        .filter(e->e.getSequence()>currentSequence)
+                        .filter(e->e.getSequence()<=requestDto.getSequence())
+                        .forEach(e->e.downSequence());
+            }else {
+                cardLists.stream()
+                        .filter(e->e.getSequence()<currentSequence)
+                        .filter(e->e.getSequence()>=requestDto.getSequence())
+                        .forEach(e->e.upSequence());
+            }
+        }
+
+        if (requestDto.getSequence()>cardLists.size()){
+
+            throw  new RuntimeException();
+        }
+
+        return CardList.toDto(cardList);
+    }
+
+    @Transactional
+    public CardListResponseDto swapSequence(Long id, UpdateCardListRequestDto requestDto) {
+        CardList cardList = cardListRepository.findByIdOrElseThrow(id);
+        CardList exchangeCardList = cardListRepository.findBySequenceAndBoardIdOrElseThrow(requestDto.getSequence(),cardList.getBoard().getId());
         Integer temp = cardList.getSequence();
         cardList.update(requestDto, exchangeCardList.getSequence());
         exchangeCardList.updateSequence(temp);
@@ -47,15 +76,18 @@ public class CardListService {
         return CardList.toDto(cardList);
     }
 
+
     @Transactional
     public void delete(Long id) {
+
         CardList cardList = cardListRepository.findByIdOrElseThrow(id);
+        Board board = boardRepository.findByIdOrElseThrow(cardList.getBoard().getId());
         Integer deleteSequence = cardList.getSequence();
         cardListRepository.delete(cardList);
-        List<CardList> cardLists = cardListRepository.findAll();
+        List<CardList> cardLists = cardListRepository.findAllByBoardId(board.getId());
         for (CardList list : cardLists) {
             if (list.getSequence() > deleteSequence) {
-                list.sortSequence(list.getSequence());
+                list.downSequence();
             }
         }
     }
