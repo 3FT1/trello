8463cbeafc69a480070b1294f23830809704824a
kangjunhyuk <kangjun1290@gmail.com>
2024-12-27 11:53:19 +0900

    Feat : 필드,생성자,어노테이션 추가
    닉네임 필드와 생성자 추가,
    @DynamicInsert, @RequiredArgsConstructor 어노테이션 추가

--- src/main/java/com/example/trello/comment/Comment.java
+++ src/main/java/com/example/trello/comment/Comment.java
@@ -4,9 +4,13 @@ import com.example.trello.card.Card;
 import com.example.trello.user.User;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
 
 @Entity
 @Getter
+@DynamicInsert
+@RequiredArgsConstructor
 public class Comment {
 
     @Id
@@ -16,10 +20,20 @@ public class Comment {
     @Column(name = "content")
     private String content;
 
+    @Column(name = "nikeName")
+    private String nikeName;
+
     @ManyToOne(fetch = FetchType.LAZY)
     private Card card;
 
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
+
+    public Comment(String content, String nikeName, Card card, User user) {
+        this.content = content;
+        this.nikeName = nikeName;
+        this.card = card;
+        this.user = user;
+    }
 
 }
