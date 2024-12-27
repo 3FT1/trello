5413a2e8dce8684d7298fb078a4efbbd33ba85e0
kangjunhyuk <kangjun1290@gmail.com>
2024-12-27 14:45:24 +0900

    Fix : 변수명 변경

--- src/main/java/com/example/trello/card/cardrepository/CardRepositoryCustom.java
+++ src/main/java/com/example/trello/card/cardrepository/CardRepositoryCustom.java
@@ -1,16 +1,10 @@
 package com.example.trello.card.cardrepository;
 
-import com.example.trello.card.Card;
-import com.example.trello.card.requestDto.CardListDto;
-import com.example.trello.card.responsedto.CardResponseDto;
-import org.springframework.data.domain.Page;
+import com.example.trello.card.requestDto.CardPageDto;
 import org.springframework.data.domain.PageRequest;
 
-import java.awt.print.Pageable;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
 
 public interface CardRepositoryCustom {
-    CardListDto searchCard(PageRequest pageRequest, Long cardListId, LocalDate startAt, LocalDate endAt, Long boardId);
+    CardPageDto searchCard(PageRequest pageRequest, Long cardListId, LocalDate startAt, LocalDate endAt, Long boardId);
 }
