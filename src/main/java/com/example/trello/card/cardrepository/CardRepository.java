afc74e7eb4170fdac552c0c684472e7a9c41ba7b
kangjunhyuk <kangjun1290@gmail.com>
2024-12-27 19:00:13 +0900

    Fix : 누락된 코드 추가

--- src/main/java/com/example/trello/card/cardrepository/CardRepository.java
+++ src/main/java/com/example/trello/card/cardrepository/CardRepository.java
@@ -1,11 +1,16 @@
 package com.example.trello.card.cardrepository;
 
 import com.example.trello.card.Card;
+import com.example.trello.cardlist.CardList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CardRepository extends JpaRepository<Card, Long>, CardRepositoryCustom {
+
+    List<Card> findByCardList(CardList cardList);
 
     default Card findByIdOrElseThrow(Long id){
         return findById(id).orElseThrow(()->new RuntimeException());
