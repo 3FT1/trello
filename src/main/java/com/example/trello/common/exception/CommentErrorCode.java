ac561ab4f07fa23be15611fd76dce1c2f413a125
kangjunhyuk <kangjun1290@gmail.com>
2024-12-31 06:31:53 +0900

    Feat : 변수 처리 추가

--- src/main/java/com/example/trello/common/exception/CommentErrorCode.java
+++ src/main/java/com/example/trello/common/exception/CommentErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum CommentErrorCode {
 
-    CANNOT_BE_MODIFIED("댓글 수정은 작성자만 가능합니다", HttpStatus.FORBIDDEN);
+    CANNOT_BE_MODIFIED("댓글 수정은 작성자만 가능합니다", HttpStatus.FORBIDDEN),
+    COMMENT_NOT_FOUND("댓글을 찾을 수 없습니다", HttpStatus.NOT_FOUND);
 
     private final String message;
     private final HttpStatus httpStatus;
